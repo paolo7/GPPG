bc0a8e7154ac2244cabfdeca34fa5e75212aa829
paolopareti <paolo.pareti@gmail.com>
2018-07-23 14:41:05 +0100

    edits

--- RuleApplicabilityTest.py
+++ RuleApplicabilityTest.py
@@ -293,7 +293,194 @@ def test4():
   test_rule_applicability(graphpattern, rules, rule5, False)
   return correct
 
-allTestsPassed = allTestsPassed & test1() & test2() & test3() & test4()
+def test5():
+  print("\nTEST SET 5\n")
+  graphpattern = {
+      hashabledict({"s": variable(1), "p": newURI("observedProperty"), "o": variable(2)}),
+      hashabledict({"s": variable(1), "p": newURI("hasFeatureOfInterest"), "o": newURI("MineElevator")}),
+      hashabledict({"s": variable(1), "p": newURI("hasResult"), "o": variable(4)}),
+    }
+  expected_graphpattern = {
+      hashabledict({"s": variable(1), "p": newURI("observedProperty"), "o": variable(2)}),
+      hashabledict({"s": variable(1), "p": newURI("hasFeatureOfInterest"), "o": newURI("MineElevator")}),
+      hashabledict({"s": variable(1), "p": newURI("hasResult"), "o": variable(4)}),
+    }
+  # the set of rules
+  rule1 = (
+          {  # Precondition:
+              hashabledict({"s": variable(1), "p": newURI("observedProperty"), "o": newURI("CO")}),
+              hashabledict({"s": variable(1), "p": newURI("hasFeatureOfInterest"), "o": newURI("TunnelEntrance")}),
+              hashabledict({"s": variable(1), "p": newURI("hasResult"), "o": variable(4)}),
+          },  # ==>
+          {  # Consequent
+              hashabledict({"s": variable(3), "p": newURI("hasCOlevel"), "o": variable(4)}),
+          }
+      )
+  rule2 = (
+      {  # Precondition:
+          hashabledict({"s": variable(1), "p": newURI("observedProperty"), "o": variable(2)}),
+          hashabledict({"s": variable(1), "p": newURI("hasFeatureOfInterest"), "o": variable(3)}),
+          hashabledict({"s": variable(1), "p": newURI("hasResult"), "o": variable(4)}),
+      },  # ==>
+      {  # Consequent
+          hashabledict({"s": variable(1), "p": newURI("hasResult"), "o": variable(4)}),
+      }
+  )
+  rules = [rule1,rule2]
+  expanded = expand_rules(graphpattern, rules, extended_debug_print)
+  correct = graph_pattern_equality(expanded,expected_graphpattern)
+  test(correct)
+  test_rule_applicability(graphpattern, rules, rule1,False)
+  test_rule_applicability(graphpattern, rules, rule2, True)
+  return correct
+
+def test6():
+  print("\nTEST SET 6\n")
+  graphpattern = {
+      hashabledict({"s": variable(1), "p": newURI("observedProperty"), "o": variable(2)}),
+      hashabledict({"s": variable(1), "p": newURI("hasFeatureOfInterest"), "o": variable(3)}),
+      hashabledict({"s": variable(1), "p": newURI("hasResult"), "o": variable(4)}),
+      hashabledict({"s": variable(1), "p": newURI("type"), "o": newURI("Room")}),
+      hashabledict({"s": variable(1), "p": newURI("type"), "o": newURI("Human")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room1")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room2")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room3")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room4")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room5")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room6")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room7")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room8")}),
+      hashabledict({"s": newURI("room8"), "p": newURI("type"), "o": newURI("humanSafeRoom")}),
+      hashabledict({"s": newURI("room6"), "p": newURI("type"), "o": newURI("humanSafeRoom")}),
+      hashabledict({"s": newURI("room5"), "p": newURI("type"), "o": newURI("humanSafeRoom")}),
+    }
+  expected_graphpattern = {
+      hashabledict({"s": variable(1), "p": newURI("observedProperty"), "o": variable(2)}),
+      hashabledict({"s": variable(1), "p": newURI("hasFeatureOfInterest"), "o": variable(3)}),
+      hashabledict({"s": variable(1), "p": newURI("hasResult"), "o": variable(4)}),
+      hashabledict({"s": variable(1), "p": newURI("type"), "o": newURI("Room")}),
+      hashabledict({"s": variable(1), "p": newURI("type"), "o": newURI("Human")}),
+      hashabledict({"s": variable(3), "p": newURI("hasMethanelevel"), "o": variable(4)}),
+      hashabledict({"s": variable(3), "p": newURI("hasRoomMethanelevel"), "o": variable(4)}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room1")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room2")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room3")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room4")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room5")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room6")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room7")}),
+      hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": newURI("room8")}),
+      hashabledict({"s": newURI("room8"), "p": newURI("type"), "o": newURI("humanSafeRoom")}),
+      hashabledict({"s": newURI("room6"), "p": newURI("type"), "o": newURI("humanSafeRoom")}),
+      hashabledict({"s": newURI("room5"), "p": newURI("type"), "o": newURI("humanSafeRoom")}),
+      hashabledict({"s": newURI("room1"), "p": newURI("containsHuman"), "o": variable(3)}),
+      hashabledict({"s": newURI("room2"), "p": newURI("containsHuman"), "o": variable(3)}),
+      hashabledict({"s": newURI("room3"), "p": newURI("containsHuman"), "o": variable(3)}),
+      hashabledict({"s": newURI("room4"), "p": newURI("containsHuman"), "o": variable(3)}),
+      hashabledict({"s": newURI("room5"), "p": newURI("containsHuman"), "o": variable(3)}),
+      hashabledict({"s": newURI("room6"), "p": newURI("containsHuman"), "o": variable(3)}),
+      hashabledict({"s": newURI("room7"), "p": newURI("containsHuman"), "o": variable(3)}),
+      hashabledict({"s": newURI("room8"), "p": newURI("containsHuman"), "o": variable(3)}),
+      hashabledict({"s": newURI("room8"), "p": newURI("RequiredAction"), "o": newURI("RestoreSafety")}),
+      hashabledict({"s": newURI("room6"), "p": newURI("RequiredAction"), "o": newURI("RestoreSafety")}),
+      hashabledict({"s": newURI("room5"), "p": newURI("RequiredAction"), "o": newURI("RestoreSafety")}),
+      hashabledict({"s": newURI("room1"), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+      hashabledict({"s": newURI("room2"), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+      hashabledict({"s": newURI("room3"), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+      hashabledict({"s": newURI("room4"), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+      hashabledict({"s": newURI("room5"), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+      hashabledict({"s": newURI("room6"), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+      hashabledict({"s": newURI("room7"), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+      hashabledict({"s": newURI("room8"), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+      hashabledict({"s": newURI("room8"), "p": newURI("hasAlertLevel"), "o": newURI("HighAlert")}),
+      hashabledict({"s": newURI("room6"), "p": newURI("hasAlertLevel"), "o": newURI("HighAlert")}),
+      hashabledict({"s": newURI("room5"), "p": newURI("hasAlertLevel"), "o": newURI("HighAlert")}),
+      hashabledict({"s": newURI("Mine"), "p": newURI("RequiredAction"), "o": newURI("FullEvacuation")}),
+    }
+  # the set of rules
+
+
+  rule6 = (
+      {  # Precondition:
+          hashabledict({"s": variable(3), "p": newURI("containsHuman"), "o": newURI("SafetyManager")}),
+          hashabledict({"s": variable(3), "p": newURI("hasAlertLevel"), "o": newURI("HighAlert")}),
+      },  # ==>
+      {  # Consequent
+          hashabledict({"s": newURI("Mine"), "p": newURI("RequiredAction"), "o": newURI("FullEvacuation")}),
+      }
+  )
+  rule5 = (
+      {  # Precondition:
+          hashabledict({"s": variable(3), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+          hashabledict({"s": variable(3), "p": newURI("RequiredAction"), "o": newURI("RestoreSafety")}),
+      },  # ==>
+      {  # Consequent
+          hashabledict({"s": variable(3), "p": newURI("hasAlertLevel"), "o": newURI("HighAlert")}),
+      }
+  )
+  rule4 = (
+      {  # Precondition:
+          hashabledict({"s": variable(3), "p": newURI("containsHuman"), "o": variable(1)}),
+          hashabledict({"s": variable(3), "p": newURI("hasRoomMethanelevel"), "o": variable(4)}),
+      },  # ==>
+      {  # Consequent
+          hashabledict({"s": variable(3), "p": newURI("RequiredAction"), "o": newURI("SendEmergencyRescue")}),
+      }
+  )
+  rule0 = (
+      {  # Precondition:
+          hashabledict({"s": variable(1), "p": newURI("hasLocation"), "o": variable(3)}),
+          hashabledict({"s": variable(3), "p": newURI("hasRoomMethanelevel"), "o": variable(4)}),
+          hashabledict({"s": variable(3), "p": newURI("type"), "o": newURI("humanSafeRoom")}),
+      },  # ==>
+      {  # Consequent
+          hashabledict({"s": variable(3), "p": newURI("RequiredAction"), "o": newURI("RestoreSafety")}),
+      }
+  )
+  rule1 = (
+      {  # Precondition:
+          hashabledict({"s": variable(3), "p": newURI("hasMethanelevel"), "o": variable(4)}),
+          hashabledict({"s": variable(3), "p": newURI("type"), "o": newURI("Room")}),
+      },  # ==>
+      {  # Consequent
+          hashabledict({"s": variable(3), "p": newURI("hasRoomMethanelevel"), "o": variable(4)}),
+      }
+  )
+  rule2 = (
+          {  # Precondition:
+              hashabledict({"s": variable(1), "p": newURI("observedProperty"), "o": newURI("Methane")}),
+              hashabledict({"s": variable(1), "p": newURI("hasFeatureOfInterest"), "o": variable(3)}),
+              hashabledict({"s": variable(1), "p": newURI("hasResult"), "o": variable(4)}),
+          },  # ==>
+          {  # Consequent
+              hashabledict({"s": variable(3), "p": newURI("hasMethanelevel"), "o": variable(4)}),
+          }
+      )
+  rule3 = (
+      {  # Precondition:
+          hashabledict({"s": variable(3), "p": newURI("hasLocation"), "o": variable(4)}),
+          hashabledict({"s": variable(4), "p": newURI("type"), "o": newURI("Room")}),
+          hashabledict({"s": variable(3), "p": newURI("type"), "o": newURI("Human")}),
+      },  # ==>
+      {  # Consequent
+          hashabledict({"s": variable(4), "p": newURI("containsHuman"), "o": variable(3)}),
+      }
+  )
+
+  rules = [rule1,rule2,rule3,rule4,rule5,rule6,rule0]
+  expanded = expand_rules(graphpattern, rules, extended_debug_print)
+  correct = graph_pattern_equality(expanded,expected_graphpattern)
+  test(correct)
+  test_rule_applicability(graphpattern, rules, rule1,True)
+  test_rule_applicability(graphpattern, rules, rule2, True)
+  test_rule_applicability(graphpattern, rules, rule3, True)
+  test_rule_applicability(graphpattern, rules, rule4, True)
+  test_rule_applicability(graphpattern, rules, rule5, True)
+  test_rule_applicability(graphpattern, rules, rule6, True)
+  test_rule_applicability(graphpattern, rules, rule0, True)
+  return correct
+
+allTestsPassed = allTestsPassed & test1() & test2() & test3() & test4() & test5() & test6()
 
 # Print of the test results
 
